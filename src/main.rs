347099ff92025b979e2eb462707b75ef4945f143
Eduard Bondarenko <1044+edbond@users.noreply.github.com>
2023-05-20 23:22:47 +0300

    some text

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::future::Future;
-
-use js_sys::Function;
+use serde_derive::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::{Document, FileReader, HtmlElement, HtmlInputElement, window, Window};
 use web_sys::console::*;
+use web_sys::{window, Document, FileReader, HtmlElement, HtmlInputElement, Window};
 use yew::prelude::*;
 
 use crate::utils::set_panic_hook;
@@ -17,26 +14,31 @@ mod utils;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Clone, Debug, PartialEq)]
+struct State {
+    value: i64,
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    set_panic_hook();
-    log_1(&"Hello, encrypt!".into());
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+struct CsvRow {
+    email: String,
+    first_name: String,
+    date_of_service: String,
 }
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
+    let counter = use_state(|| State { value: 0 });
     let onclick = {
         let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
+
+        move |ev: MouseEvent| {
+            ev.prevent_default();
+            log_1(&"Hello, encrypt, inc!".into());
+
+            counter.set(State {
+                value: counter.value + 1,
+            });
 
             let window: Window = window().expect("should have a window in this context");
             let document: Document = window
@@ -51,37 +53,23 @@ fn App() -> Html {
                 .dyn_into()
                 .unwrap();
 
-            // Create a new FileReader object and unwrap the result
-            let file_reader = FileReader::new().unwrap();
+            if let Some(file) = file_input.files().unwrap().get(0) {
+                let fr = &FileReader::new().unwrap();
 
-// Wrap a closure that takes no arguments and returns nothing in a Box
-// The closure captures the file_reader by cloning it
-// The closure logs the result of reading the file as a string
-            let onload_fn = Closure::wrap(Box::new(move || {
-// Get the result of the file_reader and unwrap it
-                let result = file_reader.clone().result().unwrap();
-// Convert the result to a string and unwrap it
-                let result = result.as_string().unwrap();
-// Log the file content using the log_1 function
-                log_1(&format!("file content: {}", result).into());
-            }) as Box<dyn FnMut()>);
+                let cb = Closure::once_into_js(move |event: Event| {
+                    // do nothing
+                    log_1(&"Hello, encrypt, onloadend!".into());
+                    let fr = event.target().unwrap().dyn_into::<FileReader>().unwrap();
+                    let result = fr.result().unwrap();
+                    let result_str = result.as_string().unwrap();
+                    log_1(&result_str.into());
+                });
 
-// Set the onload property of the file_reader to the closure reference
-            file_reader.set_onload(Some(onload_fn.as_ref().unchecked_ref()));
-
-// Get the first file from the file_input and unwrap it
-            let file = file_input.files().unwrap().get(0).unwrap();
-// Read the file as text and unwrap the result
-            file_reader.read_as_text(&file).unwrap();
-
-            let key_input = document
-                .get_element_by_id("key")
-                .expect("should have key input")
-                .dyn_into::<HtmlInputElement>()
-                .expect("should have key input");
-
-            let key = key_input.value();
-            log_1(&format!("key: {}", key).into());
+                fr.set_onloadend(Some(cb.unchecked_ref()));
+                fr.read_as_text(&file).unwrap();
+            } else {
+                log_1(&"No file selected".into());
+            }
         }
     };
 
@@ -93,6 +81,10 @@ fn App() -> Html {
             <input type="file" id="file" accept=".csv" />
         </label>
 
+        <br/>
+        <p>
+            { "Counter: " } { (*counter).value }
+        </p>
         <br/>
 
         <label>
@@ -117,6 +109,8 @@ fn App() -> Html {
 }
 
 fn main() {
+    set_panic_hook();
+
     let div = window()
         .expect("should have a window in this context")
         .document()
@@ -127,8 +121,4 @@ fn main() {
         .expect("should have an element with `id=\"app\"`");
 
     yew::Renderer::<App>::with_root(div.into()).render();
-
-    // yew::Renderer::<App>::new().render();
-    // yew::start_app::<App>();
-    // yew::start_app_in_element(div, App::init);
 }
